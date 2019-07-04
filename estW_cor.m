4d75990857aa56500b77294b83c9fdde4512a877
thisisyichenzhang <zhangyichen93@gmail.com>
2019-07-03 17:10:47 -0700

    spacing

--- estW_cor.m
+++ estW_cor.m
@@ -7,8 +7,8 @@ function W = estW_cor(X,S,Wo)
 % Initilization of W.    
 %   W = randn(m,d);
 %   W = Wo+randn(m,d);
-   W = double(Wo~=0);
-%   W = double(Wo>0.25);
+%   W = double(Wo~=0);
+   W = double(Wo>0.25);
     L=X*W;
     for k=1:K
         for i=1:d
