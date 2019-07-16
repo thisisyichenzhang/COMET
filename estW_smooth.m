55857d17ec70efeddb96434eeefffda9187ea2e1
thisisyichenzhang <zhangyichen93@gmail.com>
2019-07-16 16:58:25 -0700

    update the max evaluations times

--- estW_smooth.m
+++ estW_smooth.m
@@ -9,12 +9,12 @@ function [W,criterion] = estW_smooth(X_train,X_test,S,Wo,alpha)
 %  W = Wo+randn(m,d);
 %  W = double(Wo~=0);
 %   W = double(Wo>0.25);
-   W = Wo;
+    W = Wo;
     L=X_train*W;
     for k=1:K
         for i=1:d
             disp(i);
-            options = optimoptions('fmincon','Display','iter');
+            options = optimoptions('fmincon','Display','iter','MaxFunEvals',1e5);
             ind = W(:,i)~=0; 
             fun=@(w)(sum((corr(X_train(:,ind)*w,[L(:,1:i-1) L(:,i+1:end)]) - [S(i,1:i-1) S(i,i+1:end)]).^2)/(d-1) - alpha*sum(transpose(w)*corr(X_train(:,ind))*w)/(d-1));
             W(ind,i) = fmincon(fun,W(ind,i),[],[],[],[],[],[],[],options);
