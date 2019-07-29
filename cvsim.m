8f2836518979c55a1f77867350abd257ce661f65
thisisyichenzhang <zhangyichen93@gmail.com>
2019-07-29 13:10:48 -0700

    Update cvsim.m

--- cvsim.m
+++ cvsim.m
@@ -1,19 +1,19 @@
 %% Run simulation, generate data, Cross-validation, perform regularization method and original method
-function cvsim(n,m,d,repprop,L_noise,X_noise,W_noise,nsim,nfolds,seed)
+function cvsim(n,m,d,repprop,L_noise,X_noise,W_noise,nsim,nfolds)
     alphas = [0:0.01:0.1,0.2,0.5,0.8,1];
-    X_bysim = cell(nsim,1);
-    X_input_bysim = cell(nsim,1);
-    W_bysim = cell(nsim,1);
-    W_init_bysim  = cell(nsim,1);
-    Wl_bysim = cell(nsim,1);
+    %X_bysim = cell(nsim,1);
+    %X_input_bysim = cell(nsim,1);
+    %W_bysim = cell(nsim,1);
+    %W_init_bysim  = cell(nsim,1);
+    %Wl_bysim = cell(nsim,1);
     Wl_cv_bysim = cell(nsim,1);
     best_alpha_bysim = zeros(nsim,1);
-    criterion_cv_onfulldata_bysim = zeros(nsim,1);
-    criterion_old_onfulldata_bysim = zeros(nsim,1);
+    %criterion_cv_onfulldata_bysim = zeros(nsim,1);
+    %criterion_old_onfulldata_bysim = zeros(nsim,1);
     b = m/d;
-    % Random seed.
-    rng(seed);
     for l = 1:nsim
+        %Set the seed to be iterations number at the beginning of each iteration.
+        rng(l);
         X = zeros(n,m);
         W = zeros(m,d);
         % Simulate the dataset
@@ -38,7 +38,7 @@ function cvsim(n,m,d,repprop,L_noise,X_noise,W_noise,nsim,nfolds,seed)
         %S = corr(L);
         L_input = L + L_noise .* randn(n,d);
         S_input = corr(L_input);
-        X_input = X + X_noise .* rand(n,m);
+        X_input = X + X_noise .* randn(n,m);
 
         % Perform Cross-validation
         mean_criterion_byalpha = zeros(length(alphas),1);
@@ -62,24 +62,24 @@ function cvsim(n,m,d,repprop,L_noise,X_noise,W_noise,nsim,nfolds,seed)
     fprintf('Best alpha is found to be %0.2f\n', best_alpha);  
 
     % Runing on the full data with best tuning parameter found by CV
-    [Wl_cv,criterion_cv_onfulldata] = estW_smooth(X_input,X_input,S_input,W_init,best_alpha);
-    [Wl,criterion_old_onfulldata] = estW(X_input,S_input,W_init);
+    [Wl_cv,~] = estW_smooth(X_input,X_input,S_input,W_init,best_alpha);
+    %[Wl] = estW(X_input,S_input,W_init);
 
     fprintf('Completed for Simulation %i\n', l); 
-    X_bysim{l} = X;
-    X_input_bysim{l} = X_input;
-    W_bysim{l} = W;
-    W_init_bysim{l}  = W_init;
-    Wl_bysim{l} = Wl;
+    %X_bysim{l} = X;
+    %X_input_bysim{l} = X_input;
+    %W_bysim{l} = W;
+    %W_init_bysim{l}  = W_init;
+    %Wl_bysim{l} = Wl;
     Wl_cv_bysim{l} = Wl_cv;
     best_alpha_bysim(l) = best_alpha;
-    criterion_cv_onfulldata_bysim(l) = criterion_old_onfulldata;
-    criterion_old_onfulldata_bysim(l) = criterion_cv_onfulldata;
+    %criterion_cv_onfulldata_bysim(l) = criterion_cv_onfulldata;
+    %criterion_old_onfulldata_bysim(l) = criterion_old_onfulldata;
     disp('Results saved.');
     end
 
-    fname = sprintf('results/cv/simres_all_n%d_m%d_d%d_repprop%.2f_Lnoise%.2f_Xnoise%0.2f_Wnoise%0.2f_nsim%d_seed%d.mat', n, m, d, repprop, L_noise, X_noise, W_noise, nsim, seed);
-    save(fname,'X_bysim','X_input_bysim','W_bysim','W_init_bysim','Wl_bysim','Wl_cv_bysim','best_alpha_bysim','criterion_cv_onfulldata_bysim','criterion_old_onfulldata_bysim','alphas','nfolds','-v7.3');
+    fname = sprintf('results/cv/simres_all_n%d_m%d_d%d_repprop%.2f_Lnoise%.2f_Xnoise%0.2f_Wnoise%0.2f_nsim%d.mat', n, m, d, repprop, L_noise, X_noise, W_noise, nsim);
+    save(fname,'Wl_cv_bysim','best_alpha_bysim','alphas','nfolds','-v7.3');
     disp('Success, All Results saved!');
 
 end
