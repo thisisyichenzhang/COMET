62fcab1bde3bafaed2fc053f2984cebe31ef0585
thisisyichenzhang <zhangyichen93@gmail.com>
2019-07-29 13:11:24 -0700

    Update estW.m

--- estW.m
+++ estW.m
@@ -1,5 +1,5 @@
 % Function
-function [W,criterion] = estW(X,S,Wo)
+function [W] = estW(X,S,Wo)
     K = 1; % Number of optimization iterations
 %    [n,m] = size(X);
     d = size(S,1);
@@ -13,12 +13,13 @@ function [W,criterion] = estW(X,S,Wo)
     for k=1:K
         for i=1:d
             disp(i);
-            options = optimoptions('fmincon','Display','iter');
+            options = optimoptions('fmincon','Display','iter','MaxFunEvals',1e5);
             ind = W(:,i)~=0;
+            % abs() before sum
             fun=@(w)(-1*sum(([S(i,1:i-1) S(i,i+1:end)].*corr(X(:,ind)*w,[L(:,1:i-1) L(:,i+1:end)])))/d);
             W(ind,i) = fmincon(fun,W(ind,i),[],[],[],[],[],[],[],options);
             L=X*W;
         end
     end
-    criterion=mean(diag(transpose(W)*corr(X)*W)); 
+    %criterion=mean(diag(transpose(W)*corr(X)*W)); 
 end
